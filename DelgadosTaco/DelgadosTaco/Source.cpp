e1dbc4d970d80408303605971a72bcf45d848508
Burton428 <ADAMBURTON428@users.noreply.github.com>
2018-09-18 10:49:04 -0500

    update

--- DelgadosTaco/DelgadosTaco/Source.cpp
+++ DelgadosTaco/DelgadosTaco/Source.cpp
@@ -4,49 +4,76 @@ using namespace std;
 
 int main()
 {
-	string input;
-	int tacoTotal;
-	int burritosTotal;
-	int beansTotal;
-	int tostadasTotal;
-	int drinkTotal;
-	int tacoOrder;
-	int burritosOrder;
-	int beansOrder;
-	int tostadasOrder;
-	int drinkOrder;
+	int input;
+	double  tacoTotal;
+	double  burritosTotal;
+	double  beansTotal;
+	double tostadasTotal;
+	double  drinkTotal;
+	int tacoOrder = 0;
+	int burritosOrder=0;
+	int beansOrder = 0;
+	int tostadasOrder = 0;
+	int drinkOrder = 0;
 	double Total;
+	do{
+		cout << "What would you like to order? \n1- Taco \n2- Burritos \n3- Drink \n4- Beans \n5- Tostadas";
+		cin >> input;
+
+		if (input == 1) 
+		{
+			cout << "How many tacos would you like: ";
+			cin >> tacoOrder;
+			input = 0;
+		}
+		else if (input == 2) 
+		{
+			cout << "How many burritos would you like: ";
+			cin >> burritosOrder;
+			input = 0;
+		}
+		else if (input == 3) 
+		{
+			cout << "How many drink would you like: ";
+			cin >> drinkOrder;
+			input = 0;
+		}
+		else if (input == 4) 
+		{
+			cout << "How many bowls of beans would you like: ";
+			cin >> beansOrder;
+			input = 0;
+
+		}
+		else if (input == 5)
+		{
+			cout << "How many tostadas would you like: ";
+			cin >> tostadasOrder;
+			input = 0;
+		}
+	}while (input != 6);
+
+	tacoTotal = 2.00 * tacoOrder;
+	burritosTotal = 2.25 * burritosOrder;
+	drinkTotal = 1.50 * drinkOrder;
+	beansTotal = 1.00 * beansOrder;
+	tostadasTotal = 2.50 * tostadasOrder;
+
+	Total = tostadasTotal + beansTotal + drinkTotal + tacoTotal + burritosTotal;
 	
-	cout << "What would you like to order?";
-	cin >> input;
 
-	if (input == "Taco") {
-		cout << "How many tacos would you like: ";
-		cin >> tacoOrder;
-	}
-	else if (input == "burrito") {
-		cout << "How many burritos would you like: ";
-		cin >> burritosOrder;
-	}
-	else if (input == "drink") {
-		cout << "How many drink would you like: ";
-		cin >> drinkOrder;
-	}
-	else if (input == "bean") {
-		cout << "How many bowls of beans would you like: ";
-		cin >> beansOrder;
-	}
-	else if (input == "tostada") {
-		cout << "How many tostadas would you like: ";
-		cin >> tostadasOrder;
-	}
+	cout << "Your total is: " << Total << endl;
+
+
 
 
 	
 
 	
 
+	system("pause");
 
+	return 0;
 	
 
 }
